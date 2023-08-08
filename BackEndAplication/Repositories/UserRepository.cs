46103bc8dd4590948a00cb65d83f04dfa019d1ad
Igor Afonso <igor.henrique.afonso99@gmail.com>
2023-08-08 16:28:14 +0000

    CommitWithTokenAuth

---
+++ BackEndAplication/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using BackEndAplication.Models;
+
+namespace BackEndAplication.Repositories
+{
+    public static class UserRepository
+    {
+        public static Users Get(string username, string password)
+        {
+            var users = new List<Users>();
+            users.Add(new Users { Id = 1, Username = "batman", Password = "batman", Role = "manager" });
+            users.Add(new Users { Id = 2, Username = "robin", Password = "robin", Role = "employee" });
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
+        }
+    }
+}
---
+++ BackEndAplication/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace BackEndAplication
+{
+    public static class Settings
+    {
+        public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
+    }
+}
