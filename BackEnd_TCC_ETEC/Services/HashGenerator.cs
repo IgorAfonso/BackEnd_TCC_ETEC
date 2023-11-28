744283cd17b498b78d334d652de75bb5fa773540
Igor Afonso <igor.henrique.afonso99@gmail.com>
2023-11-28 20:11:51 -0300

    SHA256

--- BackEnd_TCC_ETEC/Services/HashGenerator.cs
+++ BackEnd_TCC_ETEC/Services/HashGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BackEnd_TCC_ETEC.Services
 {
@@ -7,17 +8,27 @@ namespace BackEnd_TCC_ETEC.Services
     {
         public string HashCreator(string pass)
         {
-            var md5 = MD5.Create();
-            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(pass);
-            byte[] hash = md5.ComputeHash(bytes);
+            //var md5 = MD5.Create();
+            //byte[] bytes = System.Text.Encoding.ASCII.GetBytes(pass);
+            //byte[] hash = md5.ComputeHash(bytes);
 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++)
+            //StringBuilder sb = new StringBuilder();
+            //for (int i = 0; i < hash.Length; i++)
+            //{
+            //    sb.Append(hash[i].ToString("X2"));
+            //}
+
+            //return sb.ToString();
+
+            byte[] bytes = Encoding.UTF8.GetBytes(pass);
+            var hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
             {
-                sb.Append(hash[i].ToString("X2"));
+                hashString += String.Format("{0:x2}", x);
             }
-
-            return sb.ToString();
+            return hashString;
         }
     }
 }
